7ba6ae74e4ef8fc9605342da314a82d76aa691f9
tylerellison16 <tylerellison16@gmail.com>
2018-06-14 09:52:32 -0400

    cheese trivia initial commit

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+
+  $("#start-button").click(function(){
+
+    var number = 60;
+    alert("You have 60 seconds, GO!");
+      
+    // start game button 
+    $("#start-button").on("click", start);  
+
+    // finish game button 
+    $("#submit").on("click", finish);  
+
+    // restart game function
+    $("#restart").on("click", restart); 
+// ----------------------------------------------------------------
+// functions
+    function start(){
+    	counter = setInterval(timer, 1000);
+    	showMe(".question");
+    	showMe(".answers");
+    	showMe("#submit");
+	    hideMe("#start-button");
+	    hideMe("#restart");
+	    hideMe("#results");
+    }
+    function timer(){
+      number-- // decrements the timer by 1
+      $("#show-number").html("<h2>" + number + "</h2>" );
+      if (number === 0){
+        alert("Times Up!")
+        stop(); // calls the stop function
+      }
+    }
+    function stop(){
+    	clearInterval(counter); // stops the timer
+    	$("#results").show();
+    	$("#restart").show();
+		$(".question").hide();
+		$(".answers").hide();
+		$("#submit").hide();
+    }
+    function finish(){
+    	number = 1; // if number is equal to 0 number will show -1 so 1 has to be selected
+    	clearInterval(counter); // stops the timer
+    	timer();
+    }
+
+    function restart(){
+    	number = 50;
+    	start();
+    }
+
+    function hideMe(e) {
+    	$(e).hide();
+    }
+    function showMe(e) {
+    	$(e).show();
+    }
+
+// ----------------------------------------------------------------
+//calling functions
+  	start(); // calls the start function
+  });
+});
